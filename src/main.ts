33094d1ad0df193c475a2cd111608d3b7e2650c1
Leandro Arturi <lea.arturi@gmail.com>
2022-12-19 22:52:29 -0300

    Inicio

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Single Responsibility Principle
+import './01-srp';
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Clean Code y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
